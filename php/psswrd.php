39d5c1e45a854101214971f8d13d6f093639b7a0
Psycho345 <psychosiabada@gmail.com>
2018-01-15 22:45:33 +0100

    Fixed hash conversion and isolated iterations

--- php/psswrd.php
+++ php/psswrd.php
@@ -11,18 +11,27 @@ class PSSWRD
 		return $str;
 	}
 
-	public function generatePassword($service, $password, $secret, $realm = "", $length = 18) {
+	public function generatePassword($service, $password, $secret, $realm = "", $length = 18, $iterations = 10000) {
 		$hash = $service . $password . $secret . $realm;
-		for ($i = 0; $i < 10000; $i++) {
+		for ($i = 0; $i < $iterations; $i++) {
 			$hash = hash("sha512", $hash);
 		}
-		$num =  sprintf("%0.0f", hexdec($hash));
+		$num = $this->hexToDec($hash);
 		$charsNum = (string)strlen($this->chars);
 		$str = "";
 		for ($i = 0; $i < $length; $i++) {
 			$idx = (int)bcmod($num, $charsNum);
 			$num = bcdiv($num, $charsNum);
 			$str = $this->chars[$idx] . $str;
+		}
+		return $str;
+	}
+
+	private function hexToDec($hex) {
+		$str = "";
+		$length = strlen($hex);
+		for ($i = 0; $i < $length; $i++) {
+			$str = bcadd($str, bcmul(strval(hexdec($hex[$i])), bcpow('16', strval($length - $i - 1))));
 		}
 		return $str;
 	}
