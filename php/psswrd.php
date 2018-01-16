8f9b4162912a3fe2f2607d0dd078b4ad1c83937e
Psycho345 <psychosiabada@gmail.com>
2018-01-16 02:40:00 +0100

    Reworked hashing method (final)

--- php/psswrd.php
+++ php/psswrd.php
@@ -2,24 +2,36 @@
 
 class PSSWRD 
 {
-	private $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+";
+	private $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+';
 
 	public function generateSecret($length = 32) {
-		$str = "";
+		$str = '';
 		for($i = 0; $i < $length; $i++) 
 			$str .= $this->chars[rand(0, $length - 1)];
 		return $str;
 	}
 
-	public function generatePassword($service, $password, $secret, $realm = "", $length = 18, $iterations = 10000) {
-		$hash = $service . $password . $secret . $realm;
-		for ($i = 0; $i < $iterations; $i++) {
-			$hash = hash("sha512", $hash);
-		}
+	public function generatePassword($service, $password, $secret, $realm = '', $realm2 = '', $maxLength = 18, $iterations = 2500) {
+		$hash = $password . $secret;
+		for ($i = 0; $i < $iterations; $i++) 
+			$hash = hash('sha512', $hash);
+		$hash = $service . $this->hashToChars($hash);
+		for ($i = 0; $i < $iterations; $i++)
+			$hash = hash('sha512', $hash);
+		$hash = $this->hashToChars($hash) . $realm;
+		for ($i = 0; $i < $iterations; $i++)
+			$hash = hash('sha512', $hash);
+		$hash = $this->hashToChars($hash) . $realm2;
+		for ($i = 0; $i < $iterations; $i++)
+			$hash = hash('sha512', $hash);	
+		return $this->hashToChars($hash, $maxLength);
+	}
+
+	private function hashToChars($hash, $maxLength = 0) {
 		$num = $this->hexToDec($hash);
 		$charsNum = (string)strlen($this->chars);
-		$str = "";
-		for ($i = 0; $i < $length; $i++) {
+		$str = '';
+		while ($num != '0' && ($maxLength <= 0 || strlen($str) < $maxLength)) {
 			$idx = (int)bcmod($num, $charsNum);
 			$num = bcdiv($num, $charsNum);
 			$str = $this->chars[$idx] . $str;
@@ -28,7 +40,7 @@ class PSSWRD
 	}
 
 	private function hexToDec($hex) {
-		$str = "";
+		$str = '';
 		$length = strlen($hex);
 		for ($i = 0; $i < $length; $i++) {
 			$str = bcadd($str, bcmul(strval(hexdec($hex[$i])), bcpow('16', strval($length - $i - 1))));
